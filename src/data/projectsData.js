df6011bb3a95bdcb842eb2f63f77a3f5edf68024
vijay-vj5 <105633955+vijay-vj5@users.noreply.github.com>
2023-03-16 18:22:26 +0530

    Updated projectdatas

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,26 +1,33 @@
-import eight from '../assets/svg/projects/eight.svg'
+import sample from '../assets/svg/projects/sample.svg'
+import one from '../assets/svg/projects/one.svg'
+import four from '../assets/svg/projects/four.svg'
 import seven from '../assets/svg/projects/seven.svg'
-import six from '../assets/svg/projects/six.svg'
+import eight from '../assets/svg/projects/eight.svg'
+import ten from '../assets/svg/projects/ten.svg'
+import eleven from '../assets/svg/projects/eleven.svg'
+import twelve from '../assets/svg/projects/twelve.svg'
+import thirteen from '../assets/svg/projects/thirteen.svg'
+
 
 
 export const projectsData = [
     {
         id: 1,
+        projectName: 'Memories-App',
+        projectDesc: 'A user-friendly Memories-App (similar to Facebook) developed using MERN stack, It is used to create,update and to delete a post and the user can add like and comments to the post .',
+        tags: ['ReactJs','NodeJs','ExpressJs','MongoDB'],
+        code: 'https://github.com/vijay-vj5/Memories-Front-End',
+        demo: 'https://memories-fb-clone.netlify.app/posts',
+        image: four
+    },
+    {
+        id: 2,
         projectName: 'Quiz App ',
         projectDesc: 'In this Quiz App, you can create your own quizzes and start answering questions. The score will be displayed based on your correct answers.',
         tags: ['ReactJs'],
         code: 'https://github.com/vijay-vj5/Quizz-App/tree/master',
         demo: 'https://qizz-app.netlify.app/',
-        image: six
-    },
-    {
-        id: 2,
-        projectName: 'My Blog',
-        projectDesc: 'This My Blog application is built using React.js and allows users to see articles, posts and write down the comments effectively. It provides a user-friendly interface for reading  information and commenting on them .',
-        tags: ['ReactJs'],
-        code: 'https://github.com/vijay-vj5/MY-BLOG-Frontend',
-        demo: 'https://my-blog-app-fe.netlify.app/',
-        image: seven
+        image: twelve
     },
     {
         id: 3,
@@ -29,15 +36,62 @@ export const projectsData = [
         tags: ['NodeJs','ExpressJs','MongoDB'],
         code: 'https://github.com/vijay-vj5/BackendCapstoneProjekt',
         demo: 'https://almabetter-project2.onrender.com/',
-        image: eight
+        image: thirteen
     },
     {
-        id: 4,
-        projectName: 'Weather App',
-        projectDesc: 'A user-friendly Weather App developed using React, offering climate details such as sunny, rainy,foggy, etc., and climate tracking.',
+        id: 2,
+        projectName: 'My Blog',
+        projectDesc: 'This My Blog application is built using React.js and allows users to see articles, posts and write down the comments effectively. It provides a user-friendly interface for reading  information and commenting on them .',
+        tags: ['ReactJs'],
+        code: 'https://github.com/vijay-vj5/MY-BLOG-Frontend',
+        demo: 'https://my-blog-app-fe.netlify.app/',
+        image: ten
+    },
+    
+   
+    {
+        id: 5,
+        projectName: 'Budget-Allocation-App ',
+        projectDesc: 'In this Budget-Allocation App, you can create your own budget and start calculating the expenses and the remainig amount . The amount will be displayed based on your budget.',
+        tags: ['NodeJs','ExpressJs','MongoDB'],
+        code: 'https://github.com/vijay-vj5/Budget-Allocation-App',
+        demo: 'https://budget-allocation-app.netlify.app/',
+        image: seven
+    },
+    {
+        id: 6,
+        projectName: 'Book Review Application ',
+        projectDesc: 'Book Review Application is used to review the books, add new books to the library.you can add your own comments and start reading it. In this app you can add, update and delete the reviews.',
+        tags: ['ReactJs'],
+        code: 'https://github.com/vijay-vj5/Book-Review-Application',
+        demo: '',
+        image: one
+    },
+    {
+        id: 7,
+        projectName: 'Weather App ',
+        projectDesc: 'By using this Weather App, you can see your location weather conditions. The Weather app allows you to detect the weather conditons for the upcoming and previous days.',
         tags: ['ReactJs'],
         code: 'https://github.com/vijay-vj5/Weather-App-React',
         demo: 'https://weather-app-reactjs-vj.netlify.app/',
-        image: six
+        image: eight
+    },
+    {
+        id: 8,
+        projectName: 'Facebook ',
+        projectDesc: 'Facebook App, this is a user friendly social media application enables you to interact with the others. it will help us to share our thoughts on the internet.',
+        tags: ['ReactJs','NodeJs','ExpressJs','MongoDB'],
+        code: 'https://github.com/vijay-vj5/Facebook-FE',
+        demo: 'https://facebook-cloneapp.netlify.app/',
+        image: sample
+    },
+    {
+        id: 9,
+        projectName: 'Notes App ',
+        projectDesc: 'In this Notes App, you can create your own todos and start working on it. The notes will be displayed based on your commends.',
+        tags: ['ReactJs'],
+        code: 'https://github.com/vijay-vj5/Notes-App',
+        demo: 'https://simple-react-notes-app.netlify.app/',
+        image: eleven
     },
 ]
